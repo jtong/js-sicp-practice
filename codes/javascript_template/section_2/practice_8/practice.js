d61bcea91ba9035b27ef4395a64a7ce8ca5c1b89
xyooyy <xyooyy@gmail.com>
2014-09-12 17:16:56 +0800

    第八题

--- codes/javascript_template/section_2/practice_8/practice.js
+++ codes/javascript_template/section_2/practice_8/practice.js
@@ -2,7 +2,7 @@ function  slove_iter(check, iterator)
 {
     return function(fun,x,dx)
     {
-        if (!check(fun,x,dx)) return slove_iter(check, iterator, fun, iterator(fun,x,dx));
+        if (!check(fun,x,dx)) return slove_iter(check, iterator)(fun, iterator(fun,x,dx),dx);
         else return x;
     }
 }
