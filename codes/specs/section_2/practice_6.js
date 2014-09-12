e63a2d83a43510bb84302391a48ecb4bbdc7b38f
xyooyy <xyooyy@gmail.com>
2014-09-12 15:11:21 +0800

    修改了练习6的测试

--- codes/specs/section_2/practice_6.js
+++ codes/specs/section_2/practice_6.js
@@ -6,7 +6,7 @@ describe('practice-1-6', function () {
         console.log("slove function, x^2 = 4, answer is 2");
         var answer_x = slove(function(x){return x*x - 4;},0.001);
         console.log("your function exec, answer is " + answer_x + "diff is " + answer_x*answer_x-4);
-        expect(answer_x*answer_x - 4 < 0.001).toBe(true);
+        expect(Math.abs(answer_x*answer_x - 4) < 0.001).toBe(true);
     });
 
 
