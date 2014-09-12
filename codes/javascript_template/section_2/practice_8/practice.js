372dbbf92b5acfbe640e7e3cf1f7be887fe50b96
xyooyy <xyooyy@gmail.com>
2014-09-12 17:34:22 +0800

    增加了 8的测试

--- codes/javascript_template/section_2/practice_8/practice.js
+++ codes/javascript_template/section_2/practice_8/practice.js
@@ -1,8 +1,10 @@
-function  slove_iter(check, iterator)
+function  slove_iter(check, buzz)
 {
+    console.log("slove_iter");
     return function(fun,x,dx)
     {
-        if (!check(fun,x,dx)) return slove_iter(check, iterator)(fun, iterator(fun,x,dx),dx);
+        console.log("function ",x,dx);
+        if (!check(fun,x,dx)) return slove_iter(check, buzz)(fun, buzz(fun,x,dx),dx);
         else return x;
     }
 }
--- codes/specs/section_2/practice_8.js
+++ codes/specs/section_2/practice_8.js
@@ -1,14 +1,25 @@
 
 describe('practice-1-6', function () {
 
-    function diff_for_test (x)
+    function diff_for_test (fun,x,dx)
     {
-        return (fun(x+0.0001)-fun(x))/0.0001;
+        return (fun(x+dx)-fun(x))/dx;
+    }
+
+    function check (fun, x, dx)
+    {
+        if (dx > Math.abs(fun(x))) return true;
+        return false;
+    }
+
+    function buzz (fun, x,dx)
+    {
+        return x -  fun(x)/diff_for_test(fun,x,dx);
     }
 
     it("Newton's method for finding roots", function () {
         console.log("slove function, x^2 = 4, answer is 2");
-        var answer_x = slove_iter(function(x){return x*x - 4;},diff_for_test,4,0.001);
+        var answer_x = slove_iter(check,buzz)(function(x){return x*x -4},5,0.001);
         console.log("your function exec, answer is " + answer_x );
         expect(Math.abs(answer_x*answer_x - 4) < 0.001).toBe(true);
     });
