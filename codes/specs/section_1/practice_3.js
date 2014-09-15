b0a642433e9bf7244c54d9ea5c416ca8d8eb63b7
jtong <tj19832@gmail.com>
2014-09-15 11:09:55 +0800

    jtong - add count change desc answer

--- codes/specs/section_1/practice_3.js
+++ codes/specs/section_1/practice_3.js
@@ -5,7 +5,7 @@ describe('practice-1-2', function () {
 
     it("todo:should throw exception when n < 0", function () {
         var dollars = [1,5,10,25,50];
-        console.log(count_change_one_dollar(100,dollars));
+        console.log(count_change(100));
 
     });
 });
