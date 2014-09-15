ef1d5bf8b9bf6e492bfd922079be33e1e15ef681
xyooyy <xyooyy@gmail.com>
2014-09-15 18:55:14 +0800

    增加section1 第三个的答案

--- codes/javascript_template/section_1/practice_3/practice.js
+++ codes/javascript_template/section_1/practice_3/practice.js
@@ -1,30 +1,6 @@
 
 function count_change(amount){
 
-    return cc(amount, 5);
+    return ;
 }
 
-function cc(amount, kind_of_coins) {
-    if(amount == 0){
-        return 1;
-    }else if(amount < 0||kind_of_coins == 0){
-        return 0
-    }else{
-        return cc(amount, kind_of_coins - 1) + cc(amount-first_denomination(kind_of_coins), kind_of_coins)
-    }
-
-
-}
-function first_denomination(kind_of_coins){
-    if(kind_of_coins == 1){
-        return 1;
-    }else if(kind_of_coins == 2){
-        return 5;
-    }else if(kind_of_coins == 3){
-        return 10;
-    }else if(kind_of_coins == 4){
-        return 25;
-    }else if(kind_of_coins == 5){
-        return 50;
-    }
-}
--- codes/specs/section_1/practice_3.js
+++ codes/specs/section_1/practice_3.js
@@ -4,9 +4,10 @@ describe('practice-1-2', function () {
 
 
     it("todo:should throw exception when n < 0", function () {
-        var dollars = [1,5,10,25,50];
-        console.log(count_change(100));
-
+        console.log("true function exec, answer is 292");
+        var cout = count_change(100);
+        console.log("your function exec, answer is "+cout);
+        expect(cout).toBe(292);
     });
 });
 
