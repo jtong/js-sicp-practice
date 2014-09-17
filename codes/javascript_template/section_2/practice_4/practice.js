4a2641d1b26d1f6a43fa23ee5b68543d7425b5e5
xyooyy <xyooyy@gmail.com>
2014-09-17 16:25:30 +0800

    修改题目

--- codes/javascript_template/section_2/practice_4/practice.js
+++ codes/javascript_template/section_2/practice_4/practice.js
@@ -1,16 +1,2 @@
-function filtered_accumulate(filter, combiner, null_value, term, a, next, b)
-{
-    if (filter_current_and_next_a(filter, next, a) > b) {
-        return null_value;
-    }
-
-    return combiner(term(filter_current_and_next_a(filter, next, a)),filtered_accumulate(filter,combiner,null_value,term,next(filter_current_and_next_a(filter ,next , a)),next,b));
-}
-
-function filter_current_and_next_a(filter ,next , a)
-{
-    if (filter(a)) {
-        return a;
-    }
-    return filter_current_and_next_a(filter, next, next(a));
+function filtered_accumulate(filter, combiner, null_value, term, a, next, b) {
 }
